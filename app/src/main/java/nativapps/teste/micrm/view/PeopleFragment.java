1838b5b007dedeaf3b8f837aded30ef618417cdd
Unknown <luniderelias@gmail.com>
2018-05-28 00:03:56 -0300

    add Person/Institute 100%

--- app/src/main/java/nativapps/teste/micrm/view/PeopleFragment.java
+++ app/src/main/java/nativapps/teste/micrm/view/PeopleFragment.java
@@ -5,10 +5,22 @@ import android.support.v4.app.Fragment;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.j256.ormlite.dao.Dao;
+
+import org.androidannotations.annotations.Background;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
+import org.androidannotations.ormlite.annotations.OrmLiteDao;
+
+import java.sql.SQLException;
 
 import nativapps.teste.micrm.R;
+import nativapps.teste.micrm.model.Institution;
+import nativapps.teste.micrm.model.Person;
+import nativapps.teste.micrm.util.ActivityUtil;
+import nativapps.teste.micrm.util.DatabaseHelper;
 
 @EFragment(R.layout.fragment_people)
 public class PeopleFragment extends Fragment {
@@ -24,5 +36,34 @@ public class PeopleFragment extends Fragment {
 
     @ViewById(R.id.addButton)
     Button addButton;
+
+    @OrmLiteDao(helper = DatabaseHelper.class)
+    Dao<Person, Integer> personDao;
+
+
+    @Click(R.id.addButton)
+    void addClick() {
+        addItem();
+    }
+
+    @Background
+    void addItem() {
+        try {
+            personDao.createOrUpdate(new Person(
+                    nameEditText.getText().toString(),
+                    phoneEditText.getText().toString(),
+                    mailEditText.getText().toString()
+            ));
+            showToast(getResources().getString(R.string.saved));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            showToast(getResources().getString(R.string.save_failed));
+        }
+    }
+
+    @UiThread
+    void showToast(String saved) {
+        ActivityUtil.showToast(getActivity(), saved);
+    }
 
 }
